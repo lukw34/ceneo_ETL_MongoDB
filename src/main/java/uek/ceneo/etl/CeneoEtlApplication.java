047577a1ad6ae6988328da1d2f00db41b428921d
lukw34 <lukw34@gmail.com>
2017-12-16 14:05:39 +0100

    Spring shell init

---
+++ src/main/java/uek/ceneo/etl/CeneoEtlApplication.java
@@ -0,0 +1,12 @@
+package uek.ceneo.etl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeneoEtlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeneoEtlApplication.class, args);
+	}
+}
